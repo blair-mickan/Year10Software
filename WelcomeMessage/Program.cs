34bc03504c3f43b45b7860dce8e65b085eb71ae4
Blair Mickan <Blair.Mickan1@education.nsw.gov.au>
2017-10-12 12:39:57 +1100

    made Hello World and Welcome Message

---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your name:");
+            String name = Console.ReadLine();
+            Console.WriteLine("welcome to my program"+ name);
+
+
+
+            // wait at the end
+            Console.ReadKey();
+        }
+    }
+}
