01991ab15f22a3c6c6b0203774e28ebdb8699e40
Blair Mickan <Blair.Mickan1@education.nsw.gov.au>
2017-10-24 14:55:31 +1100

    finished loops

---
+++ loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press enter to start");
+            Console.ReadLine();
+
+            int number = 1;
+           
+
+            while(num <= 10)
+            {
+                Console.WriteLine(num);
+                number++;
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
