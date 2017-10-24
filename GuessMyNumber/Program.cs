0ab5cb9acbda7ababcc6085fd7d9199e1606bf05
Blair Mickan <Blair.Mickan1@education.nsw.gov.au>
2017-10-25 10:38:28 +1100

    guess my numbr complete

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 101);
+            int count = 0;
+            int guess = -1;
+
+            while (guess != rand)
+            {
+                count++;
+                Console.Write("guess: ");
+                guess = int.Parse(Console.ReadLine());
+                if(guess < rand)
+                {
+                    Console.WriteLine("Too FRICKEN Low!");
+                }
+                else if (guess > rand)
+                {
+                    Console.WriteLine("Too GODAMN High!");
+                }
+                else
+                {
+                    Console.WriteLine($"you got it pussy! It took {count} tries!");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
