9c7abed69a180943022e9942aa567cbb388f5ad5
Blair Mickan <Blair.Mickan1@education.nsw.gov.au>
2017-11-21 14:59:25 +1100

    started classes

---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    class supra
+
+    {
+        private string 
+        private string
+        private int
+        private bool
+        private int TopSpeed
+    }
+}
